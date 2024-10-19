a75070b4fa81675debf67b063c999f10881f05e8
TareqJudehGithub <tareq.joudeh@gmail.com>
2024-10-19 11:27:11 +0300

    First upload from Coderz Lab, exluding old projects and files.

---
+++ Assignments/EmployeeInfo/EmployeeInfo/wwwroot/js/script.js
@@ -0,0 +1,47 @@
+﻿console.log("JS online and running!");
+
+let username = "admin";
+console.log(username);
+
+let password = "pass";
+console.log(password);
+
+let socialSecurity = .075;
+let tax = .05;
+
+// Check Login Credentials
+function CheckLogin() {  
+    let usernameInput = document.getElementById('userName');
+    let usernameValue = usernameInput.value;
+
+    let passInput = document.getElementById('password');
+    let passValue = passInput.value;
+
+    let salary = 1000;   
+    let netSalary = salary + (salary * socialSecurity) + (salary * tax);
+
+    if (usernameValue == username && passValue == password)
+    {     
+       // Apply this: window.location.href = 'emp-info.html';
+
+        let name = "John Smith";
+        alert("Welcome back, " + name);
+        document.write(
+            '<h1>Employee Details</h1>\n' +
+            '<h3>Contacts</h3>\n' +
+            '<p><strong>Name: </strong>John Smith</p>\n' +
+            '<p><strong>E-mail: </strong>example@email.com</p>\n' +
+            '<p><strong>Mobile: </strong>962 79 123456</p>\n' +
+            '<h3>Salary Details</h3>\n' +
+            '<p><strong>Salary:</strong> $' + salary + '</p>\n' +
+            '<p><strong>Social Security:</strong> 7.5%</p>\n' +
+            '<p><strong>Income Tax:</strong> 5%</p>\n' +
+            '<p><strong> Net Salary:</strong>  $' + netSalary + '</p>\n'
+        )           
+    }  
+    else
+    {
+        alert("Invalid username or password");
+    }
+}
+
---
+++ Assignments/TablesAndLists/TablesAndLists/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+let name = 'tareq';
+console.log(name);
+console.log('Hello from the Fullstack Camp!');
+
+alert('Hello World!');
+
+
+
+let name = document.getElementById('my-name').value;
