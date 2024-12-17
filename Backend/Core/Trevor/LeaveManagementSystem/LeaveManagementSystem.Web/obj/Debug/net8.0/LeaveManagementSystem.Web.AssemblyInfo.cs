9dc7e50bdcf6ca5f25c80c3254678e7b6df5cc13
TareqJudehGithub <tareq.joudeh@gmail.com>
2024-12-17 20:46:22 +0300

    Updated nav tag text color and background from _layout.cshtml

--- Backend/Core/Trevor/LeaveManagementSystem/LeaveManagementSystem.Web/obj/Debug/net8.0/LeaveManagementSystem.Web.AssemblyInfo.cs
+++ Backend/Core/Trevor/LeaveManagementSystem/LeaveManagementSystem.Web/obj/Debug/net8.0/LeaveManagementSystem.Web.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeaveManagementSystem.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a6c4f1341e90e1c8459cc40b8b1c88d7e430e20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8542412ca909d2df3ab9eeab329bcc6c9493569")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeaveManagementSystem.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeaveManagementSystem.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
